fef82e75a56b9721812ca03b99ffdf50b515406e
esyede <esyede@users.noreply.github.com>
2021-07-02 18:18:34 +0000

    phpcs: apply coding style

--- ui/layout.html.php
+++ ui/layout.html.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo (isset($title) ? _h($title) : 'Home').' | '.config('blog_title');?></title>
+    <title><?php echo(isset($title) ? _h($title) : 'Home').' | '.config('blog_title');?></title>
     <link rel="stylesheet" href="<?php echo site_url('ui/assets/css/bulma.min.css'); ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo site_url('ui/assets/css/style.css'); ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Swaagie/prismjs-monokai@master/prism-monokai.css"/>
